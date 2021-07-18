fb6a1580d1f7d24f72d12274461aae2e92f32245
Nao Komatsu <riiiin0806@gmail.com>
2021-07-18 15:25:14 +0900

    (inadequate_access_restrictions) feat: Vulnerable code

---
+++ inadequate_access_restrictions/app/src/main/java/com/example/vulnapps/inadequate_access_restrictions/WebViewActivity.java
@@ -0,0 +1,31 @@
+package com.example.vulnapps.inadequate_access_restrictions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+
+public class WebViewActivity extends AppCompatActivity {
+    private static final String TAG = "WebViewActivity";
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        Intent intent = getIntent();
+        Log.d(TAG, "onCreate: " + intent);
+
+        Uri uri = intent.getData();
+        String queryParameter = uri.getQueryParameter("url");
+        webView.loadUrl(queryParameter);
+    }
+}
